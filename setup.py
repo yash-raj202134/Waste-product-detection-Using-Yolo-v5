0d9aaf5aa735fcef99524cd98e0273cd800fe667
yash-raj202134 <yashraj3376@gmail.com>
2024-03-27 23:33:42 +0530

    setup update

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteProductDetection',
+    version = '0.0.0',
+    author = 'Yash Raj',
+    author_email = 'yashraj3376@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
+
+
+'''
+yash
+@
+raj
+
+'''
