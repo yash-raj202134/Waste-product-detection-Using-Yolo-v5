e6273ce169cacbb6fef570a70e2c16f7c4be6580
yash-raj202134 <yashraj3376@gmail.com>
2024-03-29 20:52:43 +0530

    best model added

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def predictRoute():
 
         os.system("cd yolov5/ && python detect.py --weights best.pt --img 416 --conf 0.5 --source ../data/inputImage.jpg")
 
-        opencodedbase64 = encodeImageIntoBase64("yolov5/runs/detect/exp/inputImage.jpg")
+        opencodedbase64 = encodeImageIntoBase64("data/inputImage.jpg")
         result = {"image": opencodedbase64.decode('utf-8')}
         os.system("rm -rf yolov5/runs")
 
