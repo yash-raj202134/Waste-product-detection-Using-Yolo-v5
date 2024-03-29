e66417b03eb4bde01976ba1e57ed74ffcd0a1a27
yash-raj202134 <yashraj3376@gmail.com>
2024-03-29 15:53:18 +0530

    data ingestion setup

---
+++ WasteProductDetection/entity/config_entity.py
@@ -0,0 +1,26 @@
+from WasteProductDetection.constant.training_pipeline import *
+
+import os
+from datetime import datetime
+from dataclasses import dataclass
+
+
+@dataclass
+class TrainingPipelineConfig:
+    artifacts_dir : str = ARTIFACTS_DIR
+
+
+training_pipeline_config:TrainingPipelineConfig = TrainingPipelineConfig()
+
+
+@dataclass
+class DataIngestionConfig:
+    data_ingestion_dir: str = os.path.join(
+        training_pipeline_config.artifacts_dir, DATA_INGESTION_DIR_NAME
+    )
+
+    feature_store_file_path: str = os.path.join(
+        data_ingestion_dir, DATA_INGESTION_FEATURE_STORE_DIR
+    )
+
+    data_download_url: str = DATA_DOWNLOAD_URL
